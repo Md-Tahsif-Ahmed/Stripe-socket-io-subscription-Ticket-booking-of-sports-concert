4854a10148f06c8bc09def7a782700e970b9de72
Md-Tahsif-Ahmed <tahsif.cse@gmail.com>
2026-01-28 15:39:05 +0600

    production cors

--- src/app.ts
+++ src/app.ts
@@ -47,11 +47,14 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "1mb" }));
+// app.use(express.json({ limit: "1mb" }));
 
-// app.use(express.urlencoded({ extended: true }));
+// // app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 //file retrieve
 app.use(express.static("uploads"));
